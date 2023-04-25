91ee7a03d0e0b2c66043df6972fe5b4a8d4fd4ea
Anton <anton.bryhagen@gmail.com>
2023-04-25 17:51:41 +0200

    Update user.py

--- planner/user.py
+++ planner/user.py
@@ -1,5 +1,13 @@
+import bcrypt
+
 class User:
     def __init__(self, username, name, password):
         self.username = username
         self.name = name
-        self.password = password
+        self.password = self.hash_password(password)
+    
+    def hash_password(password):
+        password = password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password, salt)
+        return hashed_password
