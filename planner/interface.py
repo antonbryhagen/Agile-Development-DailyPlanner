c49e7faa0ec593f28d4996de79363c543e9b160e
Anton <anton.bryhagen@gmail.com>
2023-04-25 20:51:00 +0200

    Integrate login and create account

--- planner/interface.py
+++ planner/interface.py
@@ -1,10 +1,12 @@
 import tkinter as tk
+import user
+import user_DAO
 
 
 class Interface:
 
     def __init__(self):
-        pass
+        self.user_DAO_handler = user_DAO.user_DAO()
 
     def display_menu(self):
         self.window = tk.Tk()
@@ -20,8 +22,9 @@ class Interface:
         header = tk.Label(text="Log in")
         header.pack()
         self.input_menu()
-        log_in = tk.Button(text="Log In")
-        log_in.pack()
+        self.log_in = tk.Button(text="Log In")
+        self.log_in.pack()
+        self.log_in.bind("<Button>", lambda event: self.get_register_data(event, "login"))
 
     def register_user_menu(self, event):
         header = tk.Label(text="Register new user")
@@ -29,10 +32,11 @@ class Interface:
         self.input_menu()
         label_name = tk.Label(text='Name:')
         label_name.pack()
-        name = tk.Entry(width=50)
-        name.pack()
-        register = tk.Button(text="Register")
-        register.pack()
+        self.name = tk.Entry(width=50)
+        self.name.pack()
+        self.register = tk.Button(text="Register")
+        self.register.pack()
+        self.register.bind("<Button>", lambda event: self.get_register_data(event, "register"))
 
 
     def input_menu(self):
@@ -41,13 +45,24 @@ class Interface:
         self.button2.destroy()
         label_username = tk.Label(text='Username:')
         label_username.pack()
-        username = tk.Entry(width=50)
-        username.pack()
+        self.username = tk.Entry(width=50)
+        self.username.pack()
         label_password = tk.Label(text='Password:')
         label_password.pack()
-        password = tk.Entry(width=50)
-        password.pack()
+        self.password = tk.Entry(width=50)
+        self.password.pack()
 
+    def get_register_data(self, event, action_type):
+        
+        user_username = self.username.get()
+        user_password = self.password.get()
+        if action_type == "register":
+            user_name = self.name.get()
+            self.user_object = user.User(user_username, user_name, user_password)
+            self.user_DAO_handler.create_user(self.user_object)
+            
+        elif action_type == "login":
+            self.user_DAO_handler.get_user_by_username(user_username, user_password)
 
     def bind_buttons(self):
         self.button1.bind("<Button>", self.log_in_menu)
