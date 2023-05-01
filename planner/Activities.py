c558e7f26d36de5b8d7e8af308d459bfc08a4b8d
Filip Danielsson <filleville@live.se>
2023-05-01 14:47:29 +0200

    Update Activities.py

--- planner/Activities.py
+++ planner/Activities.py
@@ -1,7 +1,7 @@
 class Activities:
-    """CCreate object of Activities"""
-    def __init__(self, idActivity, Activity, Time):
+    """Create object of Activities"""
+    def __init__(self, idActivity, Activity, Time, username):
         self.idActivity = idActivity
         self.Activity = Activity
         self.Time = Time
-        
+        self.username = username
