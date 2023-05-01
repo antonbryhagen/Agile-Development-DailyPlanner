9d8ed83755b3953c6a2226f8838cb1ffa15ef052
Filip Danielsson <filleville@live.se>
2023-05-01 14:18:38 +0200

    db  update
    h

---
+++ planner/Activities.py
@@ -0,0 +1,7 @@
+class Activities:
+    """CCreate object of Activities"""
+    def __init__(self, idActivity, Activity, Time):
+        self.idActivity = idActivity
+        self.Activity = Activity
+        self.Time = Time
+        
