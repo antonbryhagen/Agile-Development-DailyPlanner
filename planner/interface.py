842be433448c30cd7425e1e90ccc2e173c5f6fee
Allenalle1 <allepalle1@hotmail.com>
2023-05-01 13:08:52 +0200

    Update interface.py

--- planner/interface.py
+++ planner/interface.py
@@ -78,6 +78,7 @@ class Interface:
         welcome_text = tk.Label(text="Welcome: "+name)
         welcome_text.pack()
         self.button3 = tk.Button(text="Add activites")
+        self.button3.pack()
         welcome_window.mainloop()
 
     def bind_buttons(self):
