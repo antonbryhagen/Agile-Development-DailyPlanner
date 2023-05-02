93d5f84306d699a50afc7f3f1f69eb5a05008b16
Allenalle1 <allepalle1@hotmail.com>
2023-05-02 21:29:30 +0200

    Update interface.py

--- planner/interface.py
+++ planner/interface.py
@@ -126,6 +126,8 @@ class Interface:
         self.time = tk.Entry(width=50)
         self.time.pack()
         clicked = StringVar()
+        label_prio = tk.Label(text='Priority:')
+        label_prio.pack()
         optionList = ["Very important", "Important", "Not so important"]
         clicked.set(optionList[0])
         drop = OptionMenu(label_prio, clicked, *optionList).pack()
