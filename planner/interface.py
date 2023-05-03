3f4c40d4042c50ba644f8aedac670b2344b3b439
Filip Danielsson <filleville@live.se>
2023-05-03 11:27:34 +0200

    Update interface.py

--- planner/interface.py
+++ planner/interface.py
@@ -129,10 +129,10 @@ class Interface:
         label_prio.pack()
         clicked = StringVar(label_prio)
         optionList = ["Very important", "Important", "Not so important"]
-        clicked.get(optionList)
+        clicked.set(optionList[0])
         clicked = StringVar(optionList)
         drop = OptionMenu(label_prio, clicked, *optionList).pack()
-        clickedOption = clicked.get(optionList)
+        clickedOption = clicked.get()
         self.PRIO = clickedOption
         self.button5 = tk.Button(text="Confirm")
         self.button5.pack()
