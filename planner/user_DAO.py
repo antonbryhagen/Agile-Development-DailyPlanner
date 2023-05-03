e6e7bb5d7e43f2459fbd151604e1d5037ee06869
Filip Danielsson <filleville@live.se>
2023-05-03 12:37:15 +0200

    Update user_DAO.py

--- planner/user_DAO.py
+++ planner/user_DAO.py
@@ -69,8 +69,8 @@ class user_DAO:
         """Delete activity in database using activity object."""
         self.connect()
         cursor = self.connection.cursor()
-        activity = activity.Activity
-        create_activity_query = (f"DELETE FROM activities WHERE activity = {activity} LIMIT 1;" + "VALUES(%s)")
+        activity = activity
+        create_activity_query = ("DELETE FROM activities WHERE activity = %s LIMIT 1;  VALUES(%s)")
         create_activity_values = (activity)
         cursor.execute(create_activity_query, create_activity_values)
         self.close()
