e5ea14182e35cfd46d678fa98372538bf21e84e4
Anton <anton.bryhagen@gmail.com>
2023-05-04 09:01:17 +0200

    Add feature test.py

---
+++ test/test_features.py
@@ -0,0 +1,39 @@
+"""Feature tests for planner"""
+import unittest
+from unittest.mock import patch, Mock
+
+from planner import user_DAO
+from planner import user
+from planner import Activities
+
+
+
+class TestMain(unittest.TestCase):
+    """Class for testing main."""
+
+    def test_create_user_and_log_in(self):
+        """Test for creating a user and use it to login."""
+        
+        # Create user object as if using register form
+        self.user_username = "TestFeatureUser"
+        self.user_name = "FeatureUser"
+        self.user_password = "FeaturePass"
+        
+        self.user_DAO_handler = user_DAO.user_DAO()
+        
+        self.user_object = user.User(
+            self.user_username, self.user_name, self.user_password
+        )
+        
+        self.user_DAO_handler.create_user(self.user_object)
+        
+        # Test log in using above user as if using login form
+        
+        self.stored_username, self.stored_name = self.user_DAO_handler.get_user_by_username("TestFeatureUser", "FeaturePass")
+        self.assertEqual(self.stored_name, "FeatureUser")
+        
+                
+
+
+if __name__ == "__main__":
+    unittest.main()
